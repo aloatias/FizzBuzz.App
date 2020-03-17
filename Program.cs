d8db8fd80031f1b7f814583ab38f35cd69eaae1f
Alvaro Atias <aat@softfluent.com>
2020-03-17 20:45:20 +0100

    Final solution

--- Program.cs
+++ Program.cs
@@ -6,7 +6,21 @@ namespace FizzBuzz.App
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (var i = 1; i <= 100; i++)
+            {
+                string message = i.ToString();
+
+                if (i % 3 == 0)
+                    message = "Fizz";
+
+                if (i % 5 == 0)
+                    message = "Buzz";
+
+                if (i % 3 == 0 && i % 5 == 0)
+                    message = "FizzBuzz";
+
+                Console.WriteLine(message);
+            }
         }
     }
 }
